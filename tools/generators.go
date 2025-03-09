c23590fdaa1b96d32266c5c2db643dcee051940d
Salad109 <mateuz204@gmail.com>
2025-03-09 01:08:22 +0100

    Array generators

--- tools/generators.go
+++ tools/generators.go
@@ -13,3 +13,46 @@ func GenerateFullyRandomArray(size int) []int32 {
 	}
 	return arr
 }
+
+// GenerateSortedArray generates a sorted array of integers.
+func GenerateSortedArray(size int) []int32 {
+
+	arr := make([]int32, size)
+	for i := 0; i < size; i++ {
+		arr[i] = int32(i)
+	}
+	return arr
+}
+
+// GenerateReverseSortedArray generates a reverse sorted array of integers.
+func GenerateReverseSortedArray(size int) []int32 {
+	arr := make([]int32, size)
+	for i := 0; i < size; i++ {
+		arr[i] = int32(size - 1 - i)
+	}
+	return arr
+}
+
+// GenerateOneThirdSortedArray generates a 33% sorted array of integers.
+func GenerateOneThirdSortedArray(size int) []int32 {
+	arr := make([]int32, size)
+	for i := 0; i < size/3; i++ {
+		arr[i] = int32(i)
+	}
+	for i := size / 3; i < size; i++ {
+		arr[i] = int32(rand.Intn(10000)) // Random number between 0 and 9999
+	}
+	return arr
+}
+
+// GenerateTwoThirdsSortedArray generates a 66% sorted array of integers.
+func GenerateTwoThirdsSortedArray(size int) []int32 {
+	arr := make([]int32, size)
+	for i := 0; i < (size*2)/3; i++ {
+		arr[i] = int32(i)
+	}
+	for i := (size * 2) / 3; i < size; i++ {
+		arr[i] = int32(rand.Intn(10000)) // Random number between 0 and 9999
+	}
+	return arr
+}
